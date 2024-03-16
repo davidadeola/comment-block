25cd13649dcbcaf4763d9761689b56373feb925a
David Adeola <davidolorunsesan15@gmail.com>
2024-03-16 16:44:26 +0100

    get comment style and uncomment the block

--- src/extension.ts
+++ src/extension.ts
@@ -22,13 +22,36 @@ export function activate(context: vscode.ExtensionContext) {
         while (endLine < document.lineCount - 1 && document.lineAt(endLine).isEmptyOrWhitespace) {
             endLine++;
         }
-        vscode.window.showInformationMessage(
-            'Hello World from Awesome VSCode Extension Boilerplate!',
-        );
+
+        // Comment style used in the code
+        const commentStyle = getCommentStyle(document.lineAt(startLine).text);
+
+        // uncomment the entire block
+        vscode.window.activeTextEditor?.edit((editBuilder) => {
+            for (let line = startLine; line < endLine; line++) {
+                let text = document.lineAt(line).text.trim();
+                if (text.startsWith(commentStyle)) {
+                    text = text.slice(commentStyle.length).trim();
+                    editBuilder.replace(document.lineAt(line).range, text);
+                }
+            }
+        });
+
+        // vscode.window.showInformationMessage(
+        //     'Hello World from Awesome VSCode Extension Boilerplate!',
+        // );
     });
 
     context.subscriptions.push(disposable);
 }
 
+function getCommentStyle(line: string): string {
+    if (line.trim().startsWith('//')) {
+        return '//';
+    } else if (line.trim().startsWith('/*')) {
+        return '/*';
+    }
+    return '';
+}
 // This method is called when your extension is deactivated
 export function deactivate() {}
